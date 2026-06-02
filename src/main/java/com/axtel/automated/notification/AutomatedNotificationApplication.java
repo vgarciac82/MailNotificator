1ff9d91c12afc53b819ffbe88a9cbeeb88b4eefa
Vicente Garcia <vgarciac@axtel.com.mx>
2026-06-02 13:45:36 -0600

    Ajustar configuracion de notificaciones

--- src/main/java/com/axtel/automated/notification/AutomatedNotificationApplication.java
+++ src/main/java/com/axtel/automated/notification/AutomatedNotificationApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@org.springframework.cloud.client.discovery.EnableDiscoveryClient
 public class AutomatedNotificationApplication {
 
 	public static void main(String[] args) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 -- Configuración por defecto o stub para iniciar el servicio
 INSERT INTO smtp_configuration (host, port, username, password, protocol, active, auth_enabled, start_tls_enabled)
-VALUES ('localhost', 25, '', '', 'smtp', true, false, false);
+VALUES ('10.0.1.61', 25, '', '', 'smtp', true, false, false);
